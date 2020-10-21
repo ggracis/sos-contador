314aac6d8cad6d944d88b928ac73b0c2dfb5794c
Enzosalvo <enzosalvo1986@gmail.com>
2020-10-20 23:36:42 -0300

    agregando tabla

--- inc/sec-planes.php
+++ inc/sec-planes.php
@@ -5,8 +5,90 @@
         <div class="section m-0">
             <div class="container clearfix">
                 <div class="mx-auto center" style="max-width: 900px;">
-                    <h2>Planes Mensuales</h2></br>
-                        <h4>NO COBRAMOS LICENCIAS - ACTUALIZACIONES - NI MANTENIMIENTO</h4>
+                <table class="table table-hover table-comparison mb-0">
+					  <thead>
+						<tr>
+						  <th>&nbsp;</th>
+						  <th>Starter</th>
+						  <th>Professional</th>
+						  <th>Business</th>
+						</tr>
+					  </thead>
+					  <tbody>
+						<tr>
+						  <td>Space</td>
+						  <td>1 GB</td>
+						  <td>5 GB</td>
+						  <td>20 GB</td>
+						</tr>
+						<tr>
+						  <td>Bandwidth</td>
+						  <td>10 GB</td>
+						  <td>100 GB</td>
+						  <td>500 GB</td>
+						</tr>
+						<tr>
+						  <td>Email Accounts</td>
+						  <td>100</td>
+						  <td>1000</td>
+						  <td>5000</td>
+						</tr>
+						<tr>
+						  <td>MySQL Accounts</td>
+						  <td>100</td>
+						  <td>1000</td>
+						  <td>5000</td>
+						</tr>
+						<tr>
+						  <td>SSH Access</td>
+						  <td><i class="icon-remove"></i></td>
+						  <td><i class="icon-remove"></i></td>
+						  <td><i class="icon-ok"></i></td>
+						</tr>
+						<tr>
+						  <td>SMTP Management</td>
+						  <td><i class="icon-remove"></i></td>
+						  <td><i class="icon-remove"></i></td>
+						  <td><i class="icon-ok"></i></td>
+						</tr>
+						<tr>
+						  <td>FTP Access</td>
+						  <td><i class="icon-remove"></i></td>
+						  <td><i class="icon-ok"></i></td>
+						  <td><i class="icon-ok"></i></td>
+						</tr>
+						<tr>
+						  <td>Google Adwords Credit</td>
+						  <td><i class="icon-remove"></i></td>
+						  <td><i class="icon-ok"></i></td>
+						  <td><i class="icon-ok"></i></td>
+						</tr>
+						<tr>
+						  <td>Monthly Price</td>
+						  <td>Free</td>
+						  <td>$9.99</td>
+						  <td>$19.99</td>
+						</tr>
+						<tr>
+						  <td>Quaterly Price</td>
+						  <td>Free</td>
+						  <td>$19.99</td>
+						  <td>$49.99</td>
+						</tr>
+						<tr>
+						  <td>Yearly Price</td>
+						  <td>Free</td>
+						  <td>$69.99</td>
+						  <td>$149.99</td>
+						</tr>
+						<tr>
+						  <td>&nbsp;</td>
+						  <td><a href="#" class="btn btn-secondary">Sign Up</a></td>
+						  <td><a href="#" class="btn btn-secondary">Sign Up</a></td>
+						  <td><a href="#" class="btn btn-secondary">Sign Up</a></td>
+						</tr>
+					  </tbody>
+					</table>
                 </div>
             </div>
         </div>
