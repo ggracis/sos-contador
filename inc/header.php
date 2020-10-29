0737779864bbfb579692a13964e4b2b8ae760ee8
Gastón Gracis <gastongracis@gmail.com>
2020-10-29 00:18:34 -0300

    subiendo algunos contenidos

--- inc/header.php
+++ inc/header.php
@@ -8,9 +8,10 @@
 						<!-- Logo
 						============================================= -->
 						<div id="logo" class="mr-lg-3">
-							<a href="index.html" class="standard-logo" data-dark-logo="one-page/images/canvasone-dark.png"><img src="one-page/images/canvasone.png" alt="Canvas Logo"></a>
-							<a href="index.html" class="retina-logo" data-dark-logo="one-page/images/canvasone-dark@2x.png"><img src="one-page/images/canvasone@2x.png" alt="Canvas Logo"></a>
-						</div><!-- #logo end -->
+							<a href="index.html" class="standard-logo" data-dark-logo="images/sos-contador_157x76.png"><img src="images/sos-contador_157x76.png" alt="SOS Contador"></a>
+							<a href="index.html" class="retina-logo" data-dark-logo="images/sos-contador_157x76.png"><img src="images/sos-contador_157x76.png" alt="SOS Contador"></a>
+						</div>
+						<!-- #logo end -->
 
 						<div id="primary-menu-trigger">
 							<svg class="svg-trigger" viewBox="0 0 100 100"><path d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"></path><path d="m 30,50 h 40"></path><path d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"></path></svg>
@@ -22,24 +23,35 @@
 
 							<ul class="one-page-menu menu-container" data-easing="easeInOutExpo" data-speed="1250" data-offset="65">
 								<li class="menu-item">
-									<a href="#" class="menu-link" data-href="#wrapper"><div>Home</div></a>
-								</li>
-							
-								<li class="menu-item">
-									<a href="nosotros.php" class="menu-link"><div>Nosotros</div></a>
-								</li>
-								<li class="menu-item">
-									<a href="#" class="menu-link" data-href="#funcionalidades"><div>Funcionalidades</div></a>
-								</li>
-								<li class="menu-item">
-									<a href="#" class="menu-link" data-href="#planes"><div>Tabla de planes</div></a>
+									<a href="#" class="menu-link" data-href="#planes"><div>Planes</div></a>
 								</li>
 								<li class="menu-item">
 									<a href="#" class="menu-link" data-href="#novedades"><div>Novedades</div></a>
 								</li>
 								<li class="menu-item">
-									<a href="#" class="menu-link" data-href="#contacto"><div>Contacto</div></a>
+									<a href="nosotros.php" class="menu-link"><div>Nosotros</div></a>
+								</li>					
+								
+								<li class="menu-item">
+									<a href="#" class="menu-link" data-href="#funcionalidades"><div>REGISTRATE</div></a>
 								</li>
+								<li class="menu-item">
+									<a href="#" class="menu-link" data-href="#contacto"><div>INGRESAR</div></a>
+								</li>
+								<li class="menu-item"><div class="widget clearfix">
+								<a href="https://www.facebook.com/SOScontador/" class="social-icon si-small si-rounded si-facebook">
+								<i class="icon-facebook"></i>
+								<i class="icon-facebook"></i>
+								</a>
+								<a href="https://twitter.com/soscontador" class="social-icon si-small si-rounded si-twitter">
+								<i class="icon-twitter"></i>
+								<i class="icon-twitter"></i>
+								</a>
+								<a href="https://www.instagram.com/soscontador/" class="social-icon si-small si-rounded si-instagram">
+								<i class="icon-instagram"></i>
+								<i class="icon-instagram"></i>
+								</a>
+								</div></li>
 							</ul>
 
 						</nav><!-- #primary-menu end -->
@@ -50,3 +62,4 @@
 			<div class="header-wrap-clone"></div>
         </header>
 <!-- #header end -->
+
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include 'inc/head.php';?>
 
-<body class="stretched dark">
+<body class="stretched">
 
 	<!-- Contenedor
 	============================================= -->
@@ -12,15 +12,14 @@
 
 		<!-- Slider
 		============================================= -->
-        <?php include 'inc/sec-slider.php';?> <!-- http://themes.semicolonweb.com/html/canvas/index-corporate.html -->
+        <?php include 'inc/sec-slider.html';?> <!-- http://themes.semicolonweb.com/html/canvas/index-corporate.html -->
         <!-- #slider end -->
 
 		<!-- Contenido
 		============================================= -->
-        <?php include 'inc/sec-funcionalidades.php';?> <!-- http://themes.semicolonweb.com/html/canvas/index-corporate.html -->
-        <?php include 'inc/sec-como_funciona.php';?> <!-- Como funciona http://themes.semicolonweb.com/html/canvas/index-corporate.html-->
-        <?php include 'inc/sec-conoce_nuestro_sistemas.php';?> <!-- CONOCÉ NUESTRO SISTEMA http://themes.semicolonweb.com/html/canvas/services-2.html -->
-        <?php include 'inc/sec-empeza_hoy.php';?> <!-- Empeza hoy  http://themes.semicolonweb.com/html/canvas/index-corporate.html -->
+        <?php include 'inc/sec-como_funciona.html';?> <!-- Como funciona http://themes.semicolonweb.com/html/canvas/index-corporate.html-->
+        <?php include 'inc/sec-funcionalidades.html';?> <!-- http://themes.semicolonweb.com/html/canvas/index-corporate.html -->
+        <?php include 'inc/sec-empeza_hoy.html';?> <!-- Empeza hoy  http://themes.semicolonweb.com/html/canvas/index-corporate.html -->
         <?php include 'inc/sec-planes.php';?>    <!-- Planes  http://themes.semicolonweb.com/html/canvas/pricing.html -->                                  
         <?php include 'inc/sec-clientes.php';?> <!-- Clientes http://themes.semicolonweb.com/html/canvas/testimonials-twitter.html -->
         <?php include 'inc/sec-noticias.php';?> <!-- Noticias http://themes.semicolonweb.com/html/canvas/index.html -->
