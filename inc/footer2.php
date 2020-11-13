3376d56154d3782d0db03faa1d555db90da65798
Enzosalvo <enzosalvo1986@gmail.com>
2020-11-13 10:22:53 -0300

    ..

--- inc/footer2.php
+++ inc/footer2.php
@@ -38,10 +38,10 @@ con una permanente actualización y desarrolla nuevas funcionalidades a diario.
 
 								<div style="background: url('images/world-map.png') no-repeat center center; background-size: 100%;">
 									<address>
-										<strong>Términos y Condiciones</strong><br>
+										<strong>Términos y Condiciones</strong><br><br>
 										<strong>Defensa al Consumidor</strong><br>
-										Dirección General de Defensa<br>
-										y Protección al Consumidor<br>
+										<p>Dirección General de Defensa<br>
+										y Protección al Consumidor</p><br>
 										<a href="http://defensadelconsumidor.buenosaires.gov.ar/">Consultas y/o Denuncias</a><br>
 										<br>
 										San Francisco, CA 94107<br>
