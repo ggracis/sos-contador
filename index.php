744cb383fa91d34e079b0ad34c81a6d24c6b6df2
ggracis <gastongracis@gmail.com>
2020-12-15 15:36:39 -0300

    retoques

--- index.php
+++ index.php
@@ -26,6 +26,7 @@
                 <?php include 'inc/sec-clientes.html';?>
                 <?php include 'inc/sec-noticias.php';?> 
                 <?php include 'inc/sec-conoce_nuestros_planes.html';?>                 
+                <?php include 'inc/sec-nosotros.html';?>      
                  <?php include 'inc/sec-nuestro-adn.html';?>      
                  <?php include 'inc/sec-contacto.html';?> <!-- banner para el boton contacto -->	                                
 	             <?php include 'inc/sec-equipo.html';?>	<!-- fotos de internet-->		
