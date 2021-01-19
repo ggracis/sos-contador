e562ca286062e9efc1a5ef7bfd3c74c1d7589257
ggracis <gastongracis@gmail.com>
2021-01-19 12:09:14 -0300

    marquesina

--- inc/foot.php
+++ inc/foot.php
@@ -8,24 +8,10 @@
 	============================================= -->
 	<script src="js/jquery.js"></script>
 	<script src="js/plugins.min.js"></script>
-	<script src="js/jquery.marquee.min.js"></script>
+	
 	<script src="js/marquesina.js"></script>
 	<!-- <script src="https://maps.google.com/maps/api/js?key=YOUR-API-KEY"></script> -->
 
 	<!-- Footer Scripts
 	============================================= -->
 	<script src="js/functions.js"></script>
-
-	<script>
-		$('#noticia').hide();
-		$( document ).ready(function() {
-			setTimeout(function (){
-				$('#noticia').marquee();
-				$('#noticia').show();
-  		  console.log( "ready!" );
-			}, 500);
-						
-		});
-
-		  
-		  </script>
--- js/marquesina.js
+++ js/marquesina.js
@@ -1,27 +1,35 @@
 //const RSS_URL = 'http://www.sos-contador.com/feed/';
-const RSS_URL = './noticias.xml';
+const RSS_URL = "./noticias.xml";
 fetch(RSS_URL)
-  .then(response => response.text())
-  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-  .then(data => {
+  .then((response) => response.text())
+  .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+  .then((data) => {
     console.log(data);
     const items = data.querySelectorAll("item");
     let html = ``;
-    let i =0;
-    html+= `<span> - </span> `;
-    items.forEach(el => {
-       if (i < 5) {
-      html += `
-  
-            <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener" class="noticia-marquesina">
-              ${el.querySelector("title").innerHTML}
-            </a><span> - </span>
+    let i = 0;
+    items.forEach((el) => {
+      if (i === 1) {
+        html += `
+      <div class="carousel-item active"><p><a href="${
+        el.querySelector("link").innerHTML
+      }" target="_blank" rel="noopener" class="noticia-marquesina">
+                ${el.querySelector("title").innerHTML}</a></p></div>
+              
+        `;
+      }
+      if (i < 5 && i > 1) {
+        html += `
+    <div class="carousel-item"><p><a href="${
+      el.querySelector("link").innerHTML
+    }" target="_blank" rel="noopener" class="noticia-marquesina">
+              ${el.querySelector("title").innerHTML}</a></p></div>
             
       `;
       }
-  i++;
-    });
-//    document.body.insertAdjacentHTML("beforeend", html);
-    $( "#noticia" ).html(html);
-  });
 
+      i++;
+    });
+    //    document.body.insertAdjacentHTML("beforeend", html);
+    $("#noticia").html(html);
+  });
